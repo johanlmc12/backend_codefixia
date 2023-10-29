b6119c484cb045a1760eca593165007a4dac4de4
Farid Avila <farid.avila01@uceva.edu.co>
2023-10-29 16:46:16 -0500

    Base_datos_ok

--- esquemas.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class UsuarioRegistro(BaseModel):
-    correo: str
-    contrasena: str
--- rutas/__init__.py
+++
@@ -1,2 +0,0 @@
-# rutas/__init__.py
-from .usuario import router as usuario_router
